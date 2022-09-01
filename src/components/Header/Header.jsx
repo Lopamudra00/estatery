7e78216e443ea3fcdd22a3576e95345e4aaf6333
Lopamudra Mallick <88570040+Lopamudra00@users.noreply.github.com>
2022-09-01 11:31:39 +0530

    estate

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { nav } from "../Data";
 import './Header.css';
 
 const Header = () => {
+    const [navList] = useState(false)
     return (
         <header>
             <div className="navbar">
@@ -13,7 +15,7 @@ const Header = () => {
                     </div>
 
                     <div className="nav">
-                        <ul className="flex">
+                        <ul className={navList ? "small" : "flex"}  >
                             {nav.map((list, index) => (
                                 <li key={index}>
                                     <Link to={list.path}> {list.text} </Link>
